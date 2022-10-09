53bcc2921fc0d7cf7ff4dfee07d1966597077528
Ping Wan <ping.wan@rwth-aachen.de>
2022-10-09 23:28:45 +0200

    add bullet class and ship can fire

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys, pygame
 from settings import Settings
 from ship import Ship
 from dog import Dog
+from bullet import Bullet
 
 class AlienInvasion:
     def __init__(self) -> None:
@@ -10,10 +11,10 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                 self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.icon = pygame.image.load("images/ufo.png")
-        pygame.display.set_icon(self.icon)
+        pygame.display.set_icon(pygame.image.load("images/ufo.png"))
         self.ship = Ship(self)
         self.dog = Dog(self)
+        self.bullets = pygame.sprite.Group()
 
     
     def run_game(self):
@@ -21,10 +22,17 @@ class AlienInvasion:
         while True:
             # check events
             self._check_events()
-
             self.ship.update()
+            self._update_bullets()
             # update the screen
             self._update_screen()
+    
+    def _update_bullets(self):
+        '''update bullet location'''
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.y < 0:
+                self.bullets.remove(bullet)
     
     def _check_events(self):
         '''monitor mouse and keyboard events'''
@@ -41,6 +49,11 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:  # enter 'q' for quit
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            if len(self.bullets) < self.settings.bullet_allowed:
+                self.bullets.add(Bullet(self))
 
     def _check_keyup(self, event):
         if event.key == pygame.K_RIGHT:
@@ -55,6 +68,8 @@ class AlienInvasion:
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         self.dog.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
         
 if __name__ == "__main__":
